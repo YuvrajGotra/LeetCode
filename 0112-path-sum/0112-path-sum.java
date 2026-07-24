9d957a5977e2e85d772074f5996008db5ab50e5c
Yuvraj Tech <yuvrajgotra35@gmail.com>
2026-07-24 21:52:55 +0530

    Time: 0 ms (100%), Space: 45.2 MB (14.53%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-
         if(root.left == null && root.right == null) {
-            if(targetSum == root.val) return true;
-            else return false;
+            return targetSum == root.val;
         }
-
-        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
