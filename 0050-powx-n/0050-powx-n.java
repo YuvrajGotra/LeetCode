936ce7136891837807e2e3aa63339401345ab6f9
Yuvraj Tech <yuvrajgotra35@gmail.com>
2026-09-24 17:08:29 +0530

    Time: 1 ms (9.19%), Space: 48.4 MB (10.69%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        if(x == 1.00000) return x;
+
+        long num = n;
+        double sum = 1;
+
+        if(num < 0) {
+            x = 1/x;
+            num = -num;
+        }
+
+        while(num > 0) {
+            if(num % 2 == 1) {
+                sum *= x;
+            }
+            x *= x;
+            num /= 2;
+        }
+
+        return sum;
     }
 }
